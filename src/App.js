8730f0ec47a8d08dfdefec7b68998eee7ec62d9c
Yoseph Berhane <yosephbet@gmail.com>
2023-09-22 22:29:36 +0200

    Change API

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import "./App.css";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const API_KEY = "e667e497";
-  const BASE_URL = "http://www.omdbapi.com/?";
+  const API_KEY = "1f63e2c618fb652a0cfd55d115a7ad02";
+  const BASE_URL = "https://api.themoviedb.org/3/";
   const POPULAR_API = `${BASE_URL}movie/popular?api_key=${API_KEY}`;
-  const IMG_API = "http://img.omdbapi.com/?";
+  const IMG_API = "https://image.tmdb.org/t/p/w500";
 
   const fetchData = async (api) => {
     try {
@@ -36,14 +36,18 @@ const App = () => {
       <footer className="app-footer">
         <p>
           Powered By{" "}
-          <a href="https://www.omdbapi.com/" rel="noreferrer" target="_blank">
+          <a href="https://www.themoviedb.org" rel="noreferrer" target="_blank">
             <img
               src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
               alt="tmdb"
             />
           </a>{" "}
           | &copy; {new Date().getFullYear()} by{" "}
-          <a href="https://yoseph.dev" rel="noreferrer" target="_blank">
+          <a
+            href="https://yoseph.dev"
+            rel="noreferrer"
+            target="_blank"
+          >
             Yoseph Berhane
           </a>
         </p>
