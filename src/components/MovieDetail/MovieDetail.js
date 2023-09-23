8c5bbc510d4a369ecb03ded99a009e2c15896206
Yoseph Berhane <yosephbet@gmail.com>
2023-09-23 11:43:04 +0200

    Import useParams and useNavigate

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,48 +1,55 @@
 import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import "./MovieDetail.css";
 
-const MovieDetail = ({ match, history }) => {
+const MovieDetail = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
   const [movieDetail, setMovieDetail] = useState({});
 
-  const API_KEY = "c3ed94545b381cd59ee060f8d40ebc54";
+  const API_KEY = process.env.API_KEY;
+  
   const BASE_URL = "https://api.themoviedb.org/3/";
-  const DETAILS_API = `${BASE_URL}movie/${match.params.id}?api_key=${API_KEY}&append_to_response=videos`;
+  const DETAILS_API = `${BASE_URL}movie/:id?api_key=${API_KEY}&append_to_response=videos`;
   const IMG_API = "https://image.tmdb.org/t/p/w500";
 
-  const fetchMovieDetailData = async (api) => {
-    try {
-      const movieDetailResponse = await fetch(api);
-      const movieDetailData = await movieDetailResponse.json();
-      setMovieDetail(movieDetailData);
-    } catch (error) {
-      console.error("Error fetching movie details:", error);
-    }
+  const fetchMovieDetailData = async () => {
+    const movieDetailResponse = await fetch(
+      DETAILS_API.replace(":id", id)
+    );
+    const movieDetailData = await movieDetailResponse.json();
+
+    setMovieDetail(movieDetailData);
   };
 
   useEffect(() => {
-    fetchMovieDetailData(DETAILS_API);
-  }, [DETAILS_API]);
+    fetchMovieDetailData();
+  }, [id]); // Add the movieId parameter to the dependency array
 
-  const goBack = () => history.goBack();
-
-  const backdropStyle = movieDetail.backdrop_path
-    ? { backgroundImage: `url(${IMG_API}${movieDetail.backdrop_path})` }
-    : null;
-
-  const posterSrc = movieDetail.poster_path
-    ? `${IMG_API}${movieDetail.poster_path}`
-    : "https://images.unsplash.com/photo-1529798856831-427dfd0a1ab1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80";
-
-  const trailerLink =
-    movieDetail.videos && movieDetail.videos.results[0]
-      ? `https://www.youtube.com/embed/${movieDetail.videos.results[0].key}`
-      : null;
+  const goBack = () => {
+    navigate(-1);
+  };
 
   return (
     <div className="MovieDetail">
-      <div className="bg-image" style={backdropStyle}></div>
+      <div
+        className="bg-image"
+        style={{
+          backgroundImage: movieDetail.backdrop_path
+            ? `url(${IMG_API}${movieDetail.backdrop_path})`
+            : null,
+        }}
+      />
       <div className="content">
-        <img src={posterSrc} alt={movieDetail.title} />
+        <img
+          src={
+            movieDetail.poster_path
+              ? `${IMG_API}${movieDetail.poster_path}`
+              : "https://images.unsplash.com/photo-1529798856831-427dfd0a1ab1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+          }
+          alt={movieDetail.title}
+        />
         <div className="movie-info">
           <h2>{movieDetail.title}</h2>
           <div>
@@ -58,11 +65,17 @@ const MovieDetail = ({ match, history }) => {
           </div>
           <p>{movieDetail.overview}</p>
           <button onClick={goBack}>Go Back</button>
-          {trailerLink && (
-            <a href={trailerLink} rel="noreferrer" target="_blank">
-              <button>Play Trailer</button>
-            </a>
-          )}
+          <a
+            href={
+              movieDetail.videos && movieDetail.videos.results[0]
+                ? `https://www.youtube.com/embed/${movieDetail.videos.results[0].key}`
+                : null
+            }
+            rel="noreferrer"
+            target="_blank"
+          >
+            <button>Play Trailer</button>
+          </a>
         </div>
       </div>
     </div>
